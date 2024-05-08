352fff83b8ca56b46b892b1afecb6fbd582cefdc
t4i5e1 <astraea.nymph.08@gmail.com>
2024-05-08 22:12:59 +0900

    add 4-1f

---
+++ 4-1f.ts
@@ -0,0 +1,17 @@
+interface Car {
+    engineType: string;
+    volume: number;
+}
+interface Car {
+    color: string;
+}
+
+const myCar: Car = {
+    engineType: "V8",
+    volume: 4000,
+    color: "red"
+}
+
+const hisCar: Car = {
+    volume: 3000,
+}
